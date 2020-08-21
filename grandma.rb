47b564f3329332f1de4c39975ad6e0623ae5cd54
Anton Kalyutich <anton.kalyutich@gmail.com>
2020-08-21 03:37:28 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -17,7 +17,7 @@ def speak_to_grandma(string)
     return "I LOVE YOU TOO PUMPKIN!"
   elsif string != string.upcase
     return "HUH?! SPEAK UP, SONNY!"
-  elsif
+  elsif string != string.upcase
     return "NO, NOT SINCE 1938!"
   end
 end
